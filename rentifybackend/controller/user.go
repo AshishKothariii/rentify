22f208b18550fb8af5c81f71967cc9bc74d114bc
Ashish <93855696+AshishKothariii@users.noreply.github.com>
2024-05-28 20:22:00 +0530

    Update user.go

--- rentifybackend/controller/user.go
+++ rentifybackend/controller/user.go
@@ -79,8 +79,8 @@ func (uc *UserController) RegisterUser(c *gin.Context) {
                 if err != nil {
                         c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
                         return
-                }                             c.SetSameSite(http.SameSiteNoneMode)
-
+                }                         
+                            c.SetSameSite(http.SameSiteNoneMode)
                         c.SetCookie("token", token, 3600, "/", os.Getenv("CLIENT_URL"), false, true)
                         c.JSON(http.StatusOK,gin.H{
                                 "isLoggedin":true,
